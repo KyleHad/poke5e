298cd8edbb9b1531f050b7c4ab04c4353f4048cb
Kyle Hadwiger <kylehadwiger@neb.rr.com>
2023-09-24 19:25:03 -0500

    attempting puppeteer setup, running into an isse

---
+++ scraper/screenshot.ts
@@ -0,0 +1,11 @@
+import puppeteer from "puppeteer";
+
+async function run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://github.com');
+    await page.screenshot({ path: 'screenshots/github.png' });
+
+    browser.close();
+}
