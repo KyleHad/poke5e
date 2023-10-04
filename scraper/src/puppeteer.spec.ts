62b6f6d41bc5fead2c97fd1891cb820ca86c842a
Kyle Hadwiger <kylehadwiger@neb.rr.com>
2023-10-03 19:34:23 -0500

    finishing unit test and adding missing stuff I forgot to commit

--- scraper/src/puppeteer.spec.ts
+++ scraper/src/puppeteer.spec.ts
@@ -1,15 +1,23 @@
 import puppeteer from "puppeteer";
-
+import fs from 'fs';
 describe('puppeteer', () => {
-    it('should take a screenshot', async () => {
-        const test = 3;
+    const filePath ='src/screenshots/github.png'
 
+    afterEach(() => {
+        if (fs.existsSync(filePath)) {
+            // Delete the file using fs.unlinkSync()
+            fs.unlinkSync(filePath);
+        }
+    })
+    it('should take a screenshot', async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
         await page.goto('https://github.com');
-        await page.screenshot({ path: 'screenshots/github.png' });
+        await page.screenshot({ path: filePath, optimizeForSpeed: true });
+        await browser.close();
 
-        expect(test).toBe(3)
-    })
+
+        expect(fs.existsSync(filePath)).toBeTruthy()
+    }, 20000)
 })
